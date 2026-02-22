4f677b73b3a97d129b2107088567e0b849718847
Michael Kargl <michael.kargl@zastrpay.com>
2026-02-22 16:43:53 +0100

    feat: enables the RedstoneOutputBlock to be able to emit redstone signals

--- src/main/java/at/osa/redstonewire/RedstoneOutputBlock.java
+++ src/main/java/at/osa/redstonewire/RedstoneOutputBlock.java
@@ -2,6 +2,8 @@ package at.osa.redstonewire;
 
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
@@ -46,5 +48,15 @@ public class RedstoneOutputBlock extends Block {
         RedstoneWire.LOGGER.info("Neighbor changed for RedstoneOutputBlock at pos: {}", pos);
 
         super.neighborChanged(state, level, pos, block, fromPos, isMoving);
+    }
+
+    @Override
+    public boolean isSignalSource(BlockState state) {
+        return state.getValue(POWER) > 0;
+    }
+
+    @Override
+    public int getSignal(BlockState state, BlockGetter level, BlockPos pos, Direction direction) {
+        return state.getValue(POWER);
     }
 }
