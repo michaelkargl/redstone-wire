6fb9cc02e732ce6f0917dfa135407605287d6836
Michael Kargl <michael.kargl@posteo.net>
2026-05-14 10:20:08 +0200

    feat: include redstone dust in the redstone_wire creative tab

--- src/main/java/at/osa/redstonewire/init/ModCreativeTabs.java
+++ src/main/java/at/osa/redstonewire/init/ModCreativeTabs.java
@@ -5,6 +5,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Items;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
@@ -20,5 +21,7 @@ public final class ModCreativeTabs {
             .displayItems((parameters, output) -> {
                 // Automatically register all items to creative tab
                 ModItems.ITEMS.getEntries().forEach(item -> output.accept(item.get()));
+                // Manually register vanilla blocks
+                output.accept(Items.REDSTONE.getDefaultInstance());
             }).build());
 }
